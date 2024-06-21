5083a21f7d19d39c1ead2d631eadc440599e6c00
evranahmet <ahmetevran.ae@gmail.com>
2024-06-22 02:27:12 +0300

    22/06

---
+++ src/test/java/homeWork36.java
@@ -0,0 +1,3 @@
+public class homeWork36 {
+    //CH5_SeleniumGridCucumber projesinde bir paralel test yazınız ve 3 thread ile koşunuz.
+}
---
+++ src/test/java/tests/C02_LoginTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+
+import org.testng.annotations.Test;
+import pages.CLContactPage;
+import pages.CLHomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import static org.testng.Assert.assertEquals;
+
+public class C02_LoginTest {
+
+    @Test
+    public void loginTest01() {
+
+        CLHomePage clHomePage = new CLHomePage();
+        CLContactPage clContactPage = new CLContactPage();
+
+        //Go to url
+        Driver.getDriver().get(ConfigReader.getProperty("contact_list_url"));
+
+        //Enter email
+        clHomePage.email.sendKeys("xyz@gmail.com");
+
+        //Enter password
+        clHomePage.password.sendKeys("1234567");
+
+        //Click submit
+        clHomePage.submit.click();
+
+        //Assert Contact List Header
+        assertEquals(clContactPage.header.getText(), "Contact List");
+        assert clContactPage.logout.isDisplayed();
+
+        Driver.closeDriver();
+    }
+
+
+}
